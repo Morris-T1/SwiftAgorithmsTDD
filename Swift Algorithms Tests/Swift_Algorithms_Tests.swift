dccdf9895f9f7edcf30229cc534e576a8c1e85f1
Morris-T1 <morris.t@etoncollege.org.uk>
2022-11-22 11:10:30 +0000

    second commit

---
+++ Swift Algorithms Tests/Swift_Algorithms_Tests.swift
@@ -0,0 +1,14 @@
+//
+//  Swift_Algorithms_Tests.swift
+//  Swift Algorithms Tests
+//
+//  Created by Morris, Fenston (AGDF) on 22/11/2022.
+//
+
+import XCTest
+
+class Swift_Algorithms_Tests: XCTestCase {
+    
+    func bubbleSortTestWithUnsortedArray( )
+
+}
--- SwiftAlgorithmsTDD/main.swift
+++ SwiftAlgorithmsTDD/main.swift
@@ -1,11 +1,29 @@
-//
-//  main.swift
-//  SwiftAlgorithmsTDD
-//
-//  Created by Morris, Fenston (AGDF) on 22/11/2022.
-//
-
 import Foundation
 
-print("Testtest")
+
+func bubbleSort(data: inout [Int]) -> [Int] {
+    for i in 0...data.count - 1{
+       for j in 0...data.count - 1 {
+           if data[i] < data[j] {
+           data.swapAt(j, i)
+         }
+         
+       
+       }
+     }
+
+    return data
+    
+}
+var list = [Int]()
+for _ in 0...1000 {
+    let randomNumber = Int.random(in: 0...1000)
+    list.append(randomNumber)
+}
+        
+let timeNow = Date().timeIntervalSinceReferenceDate
+print(bubbleSort(data: &list))
+let timeTaken = Date().timeIntervalSinceReferenceDate - timeNow
+print(timeTaken)
+
 
